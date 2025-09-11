95532bc4097287e514ea642d72129d8fb01f623c
monitpontes <gemeosdigitais@cagen.com.br>
2025-09-11 10:30:04 -0300

    router ok

--- src/routes/ingest.js
+++ src/routes/ingest.js
@@ -1,9 +1,21 @@
 // src/routes/ingest.js
+import { Router } from "express";
 import { ingestAccel } from "../controllers/ingestAccel.js";
 import { ingestFrequency } from "../controllers/ingestFreq.js";
 
+const router = Router();
+
 router.post("/accel", ingestAccel);
 router.post("/frequency", ingestFrequency);
+
+export default router;
+
+
+// import { ingestAccel } from "../controllers/ingestAccel.js";
+// import { ingestFrequency } from "../controllers/ingestFreq.js";
+
+// router.post("/accel", ingestAccel);
+// router.post("/frequency", ingestFrequency);
 
 
 // import { Router } from "express";
