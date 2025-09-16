337e326fed1bb7406a231b9f98e0c5bd2b883b01
monitpontes <gemeosdigitais@cagen.com.br>
2025-09-16 16:19:52 -0300

    heartbeat

--- api/heartbeat.js
+++ api/heartbeat.js
@@ -1,6 +1,12 @@
 import { runBridgeHeartbeatOnce } from '../src/services/bridgeHeartbeat.js';
 import { connectMongo } from '../src/lib/db.js';
 
+// ADICIONAR ESTES IMPORTS:
+import '../src/models/company.js';
+import '../src/models/bridge.js';
+import '../src/models/device.js';
+import '../src/models/bridgeStatus.js';
+
 export default async function handler(req, res) {
   try {
     await connectMongo(process.env.MONGO_URI);
