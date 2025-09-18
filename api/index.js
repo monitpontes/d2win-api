aeffb47d0ff54269f3d9bef6098451b76cb746a8
monitpontes <gemeosdigitais@cagen.com.br>
2025-09-17 22:31:47 -0300

    sem cron

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 // api/index.js
 import app, { boot } from "../src/app.js";
-import handler from "./heartbeat.js";
 
 // Garante que conecta no banco antes de responder
 await boot();
 
-export default handler;
+export default app;
